bbb1f3ac9abe9e9c6ff99a57456644996fae5b63
lleaff <lleaff@users.noreply.github.com>
2015-11-02 23:10:17 +0100

    Support holes in pre-applied arguments with special object '_'

--- 1/partial.js
+++ 1/partial.js
@@ -1,6 +1,23 @@
-exports.partial = function partial(fn) {
+var __ = {};
+
+function partial(fn) {
   var args = Array.prototype.slice.call(arguments, 1);
   return function partiallyApplied() {
-    return fn.apply(this, args.concat(Array.prototype.slice.call(arguments)));
+    var newArgs = constructArgs(args, arguments);
+    return fn.apply(this, newArgs);
   };
 }
+
+function constructArgs(args1, args2) {
+  args1 = Array.prototype.slice.call(args1);
+  args2 = Array.prototype.slice.call(args2);
+  var l1 = args1.length;
+
+  for (var i = 0; args2.length || i < l1; ++i) {
+    if (i >= l1 || args1[i] === __)
+      args1[i] = args2.shift();
+  }
+  return (args1);
+}
+
+module.exports = { partial: partial, _: __ };
